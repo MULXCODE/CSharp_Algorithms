de70cf328107be392b947386a68701aef8912682
Garrett Wong <wong_ga@sc.wdc.com>
2017-01-18 20:09:38 -0800

    initial commit

---
+++ Algorithms/Algorithms/Class1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    [TestFixture]
+    [Category("Test")]
+    public class NoWay
+    {
+
+        [Test]
+        public void RunTest()
+        {
+
+        }
+    }
+}
